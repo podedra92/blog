26d456f8856bae4ff05641d0ad55fef3d874ea86
podedra92 <podedra@hotmail.com>
2022-12-23 15:52:32 +0000

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,12 +1,12 @@
 export default {
-    base: '/blog/',
-    themeConfig: {
-      socialLinks: [
-        { icon: 'linkedin', link: 'https://uk.linkedin.com/in/prakash-odedra-892183183' },
-        { icon: 'github', link: 'https://github.com/podedra92' }
-      ],
-      nav: [
-        { text: 'About Me', link: '/about/' },
-      ],
-    }
+  base: '/blog/',
+  themeConfig: {
+    socialLinks: [
+      { icon: 'linkedin', link: 'https://uk.linkedin.com/in/prakash-odedra-892183183' },
+      { icon: 'github', link: 'https://github.com/podedra92' }
+    ],
+    nav: [
+      { text: 'About Me', link: '/about/' },
+    ],
   }
+}
